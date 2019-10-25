3ce2e08eea774456c53e24a9eee6c1a6ac960f68
solidangle <obrienpja@gmail.com>
2019-10-24 21:18:39 -0500

    Attempt to make Model work for any Lattice. Broken state.

--- Model/Model.hpp
+++ Model/Model.hpp
@@ -4,8 +4,9 @@
 
 #include <armadillo>
 #include <fstream>
-#include <bits/unique_ptr.h>
+#include <bits/shared_ptr.h>
 #include "../Lattice/Square.hpp"
+#include "../Lattice/Triangular.hpp"
 
 using namespace arma;
 
@@ -14,7 +15,7 @@ using namespace arma;
 class Model {
 private:
     int system_size{};
-    Square lat;
+    std::shared_ptr<Lattice> lat;
 public:
     virtual double energy() = 0;
 
@@ -22,23 +23,23 @@ public:
 
     virtual Spin new_spin(std::mt19937 & a, std::uniform_int_distribution<int> & b) = 0;
 
-    Square & get_lattice(){return lat;};
+    std::shared_ptr<Lattice> get_lattice(){return lat;};
 
     void create_random_initial_spin_configuration(std::mt19937 & a, std::uniform_int_distribution<int> & b){
-        for (auto &m: lat.get_lattice()) {
+        for (auto &m: lat->get_lattice()) {
             Spin s(new_spin(a, b));
             m.set_spin(s);
         }
     };
 
-    void print_spin_configuration(){for(auto &m: lat.get_lattice()){m.get_spin().print_spin();}
+    void print_spin_configuration(){for(auto &m: lat->get_lattice()){m.get_spin().print_spin();}
     std::cout << std::endl;};
 
     void set_system_size(int sys){system_size = sys;};
 
     int get_system_size(){return system_size;};
 
-    void set_lattice(Square & sq){lat = sq;};
+    void set_lattice(std::shared_ptr<Lattice> sq){lat = std::move(sq);};
 
     virtual void update_spin_configuration(int ind, Spin & n_spin) = 0;
 
@@ -46,7 +47,7 @@ public:
 
     virtual void create_ferromagnetic_spin_configuration() = 0;
 
-    Model(Square & l, int sys){set_lattice(l); set_system_size(sys);};
+    Model(std::shared_ptr<Lattice> l, int sys){set_lattice(l); set_system_size(sys);};
 
 //    void set_spin_configuration()
 };
