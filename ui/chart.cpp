1b4b32af2fb09adbe5ef6d45bae1f2a915b8c6aa
jwdeitch <jwdeitch@gmail.com>
2019-12-12 23:39:42 -0500

    progress

--- ui/chart.cpp
+++ ui/chart.cpp
@@ -1,11 +1,22 @@
 #include "chart.h"
 #include "ui_chart.h"
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QtCharts>
 
 chart::chart(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::chart)
 {
     ui->setupUi(this);
+
+	auto series = new QtCharts::QLineSeries();
+	series->append(0, 6);
+	series->append(2, 4);
+	
+    auto chart_viewport = new QtCharts::QChart(nullptr);
+	chart_viewport->addSeries(series);
+    ui->graphicsView->setChart(chart_viewport);
+    
 }
 
 chart::~chart()
